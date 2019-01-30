c1fc86a97e2795da4197095522f8003994885559
Nikka Hassan <nikka.hassan@gmail.com>
2019-01-30 22:58:08 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz(int)
-  if int % 3==0 && int % 5==0
-    "FizzBuzz"
-  elsif int % 3==0
-    "Fizz"
-  elsif int % 5==0
-    "Buzz"
+  if int % 3 == 0 && int % 5 == 0
+    puts "FizzBuzz"
+  elsif int % 3 == 0
+    puts "Fizz"
+  elsif int % 5 == 0
+    puts "Buzz"
   else
-    nil
+    puts nil
   end
 end
